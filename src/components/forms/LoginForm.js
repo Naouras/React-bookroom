1a09e1dc515b1e8822832631de5a76e3a53d2163
Naoures Hadj Amor <naoures.hadjamor@ett.tn>
2018-03-09 16:30:27 +0100

    be contuned

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {Form , Button} from 'semantic-ui-react';
+
+class LoginForm extends React.Component {
+    state={
+        data: {
+            email: '',
+            password: ''
+        },
+        loadin : false,
+        errors : {}   
+    };
+    onchange(e){
+        this.setState({data: {...this.state.data, [e.target.name]:e.target.value}});
+    }
+    onSubmit = () => {
+        
+    }
+  render() {
+    const { data } = this.state;
+     return (
+    <Form onSubmit={this.onSubmit}>
+     <Form.Field>
+     <label htmlFor="email">Email</label>
+     <input type="email"
+             id="email" 
+             name="email" 
+             placeholder="example@example.com"
+             value={data.password}
+             onChange={e => this.onchange(e)}
+              />
+     </Form.Field>
+     <Form.Field>
+     <label htmlFor="password">Password</label>
+     <input type="password"
+             id="password" 
+             name="password" 
+             placeholder="make it secure"
+             value={data.email}
+             onChange={e => this.onchange(e)}
+              />
+     </Form.Field>
+    <Button primary> Login </Button>
+    </Form>
+     );
+  }
+}
+
+export default LoginForm;
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import LoginForm from '../forms/LoginForm';
 
 const LoginPage = () => <div> 
  Login Page
+ <LoginForm />
  </div>;
 
 export default LoginPage;
